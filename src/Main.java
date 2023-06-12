f531099f02635182b42be86478006c28785a65e1
Nick Adams <nick.pappas.adams@gmail.com>
2023-06-12 13:53:46 -0700

    added starter linked list methods

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,21 @@
+import java.util.HashMap;
+
 public class Main {
 
     public static void main(String[] args) {
         Cookie cookie1 = new Cookie("green");
         Cookie cookie2 = new Cookie("blue");
+        LinkedList list = new LinkedList(4);
 
         cookie1.setColor("orange");
-
         System.out.println(cookie1.getColor());
         System.out.println(cookie2.getColor());
+
+        list.printList();
+        list.getLength();
+        list.getHead();
+        list.getTail();
+
+
     }
 }
