33b98cd0a7174712d7db2f4321fb937b7658cd92
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-13 13:59:10 -0700

    LL constructor

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -2,6 +2,25 @@ package LinkedList;
 
 public class LinkedList {
 
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public LinkedList(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    static class Node {
+        int value;
+        Node next;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
 
     // append method: O(1)
     // remove last: O(n)
--- src/LinkedList/Main.java
+++ src/LinkedList/Main.java
@@ -1,4 +1,9 @@
 package LinkedList;
 
 public class Main {
+
+    public static void main(String[] args) {
+        LinkedList myLinkedList = new LinkedList(4);
+        System.out.println(myLinkedList);
+    }
 }
