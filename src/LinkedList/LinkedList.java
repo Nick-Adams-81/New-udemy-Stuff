dd6780372390719c776ff355b91e02042c3a107e
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-13 13:48:53 -0700

    linked list set up

---
+++ src/LinkedList/LinkedList.java
@@ -0,0 +1,14 @@
+package LinkedList;
+
+public class LinkedList {
+
+
+    // append method: O(1)
+    // remove last: O(n)
+    // prepend method: O(1)
+    // remove first: O(1)
+    // insert : O(n)
+    // remove item: O(n)
+    // find by value: O(n)
+
+}
