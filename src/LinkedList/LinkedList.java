7400a7a34b06eeaec3ce415878c595f364ed5d72
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-13 22:47:05 -0700

    set method

--- src/LinkedList/LinkedList.java
+++ src/LinkedList/LinkedList.java
@@ -108,6 +108,14 @@ public class LinkedList {
         return temp;
     }
 
+    public boolean set(int index, int value) {
+        Node temp = get(index);
+        if(temp != null) {
+            temp.value = value;
+            return true;
+        }
+        return false;
+    }
     // insert : O(n)
 
     // remove item: O(n)
