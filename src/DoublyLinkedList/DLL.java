733151618f0e158ee562ec029eafe7d870204077
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-15 15:07:10 -0700

    dll constructor

--- src/DoublyLinkedList/DLL.java
+++ src/DoublyLinkedList/DLL.java
@@ -2,4 +2,44 @@ package DoublyLinkedList;
 
 public class DLL {
 
+    private Node head;
+    private Node tail;
+    private int length;
+
+    public DLL(int value) {
+        Node newNode = new Node(value);
+        head = newNode;
+        tail = newNode;
+        length = 1;
+    }
+
+    class Node {
+        int value;
+        Node next;
+        Node prev;
+
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    public void printList() {
+        Node temp = head;
+        while(temp != null) {
+            System.out.println(temp.value);
+            temp = temp.next;
+        }
+    }
+
+    public void getHead() {
+        System.out.println("Head: " + head.value);
+    }
+
+    public void getTail() {
+        System.out.println("Tail: " + tail.value);
+    }
+
+    public void getLength() {
+        System.out.println("Length: " + length);
+    }
 }
--- src/DoublyLinkedList/Main.java
+++ src/DoublyLinkedList/Main.java
@@ -3,6 +3,11 @@ package DoublyLinkedList;
 public class Main {
 
     public static void main(String[] args) {
+        DLL myDLL = new DLL(1);
 
+        myDLL.printList();
+        myDLL.getHead();
+        myDLL.getTail();
+        myDLL.getLength();
     }
 }
