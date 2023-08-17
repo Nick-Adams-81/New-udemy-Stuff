699d00b0729a2f0bc13dd16290974da7bdaa47dc
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-17 16:56:23 -0700

    valid parens

--- src/InterviewQuestions.java
+++ src/InterviewQuestions.java
@@ -14,7 +14,21 @@ public class InterviewQuestions {
         }
         return reversed;
     }
+
+    // bal;anced parentheses
+    public static boolean validParentheses(String str) {
+        if(str.length() % 2 != 0) return false;
+        Stack<Character> stack = new Stack<>();
+        for(char c : str.toCharArray()) {
+            if(c == '[' || c == '{' || c == '(') stack.push(c);
+            else if(c == ']' && stack.peek() == '[') stack.pop();
+            else if(c == '}' && stack.peek() == '{') stack.pop();
+            else if(c == ')' && stack.peek() == '(') stack.pop();
+        }
+        return stack.isEmpty();
+    }
     public static void main(String[] args) {
         System.out.println(reverseString("hello world"));
+        System.out.println(validParentheses("[]()}"));
     }
 }
