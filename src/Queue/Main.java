fb20605eafde20c038772b33828e7ef80c91e65a
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-16 17:27:12 -0700

    dequeue

--- src/Queue/Main.java
+++ src/Queue/Main.java
@@ -11,6 +11,14 @@ public class Main {
         queue.printLength();
 
         queue.enqueue(3);
+        queue.enqueue(4);
+        queue.enqueue(5);
+        queue.printQueue();
+        queue.printFirst();
+        queue.printLast();
+        queue.printLength();
+
+        queue.dequeue();
         queue.printQueue();
         queue.printFirst();
         queue.printLast();
