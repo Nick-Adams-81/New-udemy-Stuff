da70bc6f7a86d112e6df3d3c57f1ea9cec4aac76
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-18 10:20:53 -0700

    bst constructor

--- src/Trees/BST.java
+++ src/Trees/BST.java
@@ -3,4 +3,20 @@ package Trees;
 public class BST {
 
 
+    private Node root;
+
+    static class Node {
+        int value;
+        Node right;
+        Node left;
+        Node(int value) {
+            this.value = value;
+        }
+    }
+
+    // lookup O(log n)
+
+    // insert O(log n)
+
+    // remove O(log n)
 }
