6ea7282b777f1a61b8e58b9bf1d4ee268c318e10
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-17 17:08:37 -0700

    sort stack

--- src/InterviewQuestions.java
+++ src/InterviewQuestions.java
@@ -15,7 +15,7 @@ public class InterviewQuestions {
         return reversed;
     }
 
-    // bal;anced parentheses
+    // balanced parentheses using stack
     public static boolean validParentheses(String str) {
         if(str.length() % 2 != 0) return false;
         Stack<Character> stack = new Stack<>();
@@ -27,7 +27,32 @@ public class InterviewQuestions {
         }
         return stack.isEmpty();
     }
+
+    // sorting a stack of integers
+    public static void sortStack(Stack<Integer> stack) {
+        Stack<Integer> sorted = new Stack<>();
+        while(!stack.isEmpty()) {
+            int temp = stack.pop();
+            while(!sorted.isEmpty() && sorted.peek() > temp) {
+                stack.push(sorted.pop());
+            }
+            sorted.push(temp);
+        }
+        while(!sorted.isEmpty()) {
+            stack.push(sorted.pop());
+        }
+    }
+
     public static void main(String[] args) {
+        Stack<Integer> myStack = new Stack<>();
+        myStack.push(7);
+        myStack.push(3);
+        myStack.push(34);
+        myStack.push(9);
+        myStack.push(1);
+        System.out.println("Unsorted: " + myStack);
+        sortStack(myStack);
+        System.out.println("Sorted: " + myStack);
         System.out.println(reverseString("hello world"));
         System.out.println(validParentheses("[]()}"));
     }
