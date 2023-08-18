b50827b9fc42356f424699eef976cb7fb68e4f1b
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-18 16:07:21 -0700

    added print table

--- src/HashTables/HashTable.java
+++ src/HashTables/HashTable.java
@@ -19,5 +19,16 @@ public class HashTable {
         }
     }
 
+    public void printTable() {
+        for(int i = 0; i < dataMap.length; i++) {
+            System.out.println(i + ":");
+            Node temp = dataMap[i];
+            while(temp != null) {
+                System.out.println("{" + temp.key + " : " + temp.value + "}");
+                temp = temp.next;
+            }
+        }
+    }
+
 
 }
