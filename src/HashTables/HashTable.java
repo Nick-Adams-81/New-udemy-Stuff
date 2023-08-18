ac393d3ffe940b135c202d508fb8a7055a410ab7
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-18 16:04:55 -0700

    hash table constructor

--- src/HashTables/HashTable.java
+++ src/HashTables/HashTable.java
@@ -2,4 +2,22 @@ package HashTables;
 
 public class HashTable {
 
+    private int size = 7;
+    private Node[] dataMap;
+
+    public HashTable() {
+        dataMap = new Node[size];
+    }
+
+    public class Node {
+        private String key;
+        private int value;
+        private Node next;
+        Node(String key, int value) {
+            this.key = key;
+            this.value = value;
+        }
+    }
+
+
 }
