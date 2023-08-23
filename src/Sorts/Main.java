b76ed61fd42755bb8cd963387aa27bc7af97ba3b
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-23 16:07:39 -0700

    selection sort

--- src/Sorts/Main.java
+++ src/Sorts/Main.java
@@ -5,10 +5,16 @@ import java.util.Arrays;
 public class Main {
 
     public static void main(String[] args) {
-        int[] nums = {92, 7, 34, 13, 21, 11, 3, 98};
 
-        System.out.println("Unsorted: " + Arrays.toString(nums));
-        BubbleSort.bubbleSort(nums);
-        System.out.println("Sorted: " + Arrays.toString(nums));
+        int[] nums1 = {92, 7, 34, 13, 21, 11, 3, 98};
+        int[] nums2 = {5, 43, 12, 11, 90, 76, 14, 123};
+
+        System.out.println("Unsorted: " + Arrays.toString(nums1));
+        BubbleSort.bubbleSort(nums1);
+        System.out.println("Bubble Sort: " + Arrays.toString(nums1));
+        System.out.println("");
+        System.out.println("Unsorted: " + Arrays.toString(nums2));
+        SelectionSort.selectionSort(nums2);
+        System.out.println("Sorted: " + Arrays.toString(nums2));
     }
 }
