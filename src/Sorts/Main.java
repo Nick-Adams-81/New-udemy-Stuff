30c3c2b5c98f2e553d087a7f9c350140b57cedfa
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-23 16:14:01 -0700

    insertion sort

--- src/Sorts/Main.java
+++ src/Sorts/Main.java
@@ -8,6 +8,7 @@ public class Main {
 
         int[] nums1 = {92, 7, 34, 13, 21, 11, 3, 98};
         int[] nums2 = {5, 43, 12, 11, 90, 76, 14, 123};
+        int[] nums3 = {54, 1, 23, 14, 76, 456, 321, 97};
 
         System.out.println("Unsorted: " + Arrays.toString(nums1));
         BubbleSort.bubbleSort(nums1);
@@ -16,5 +17,9 @@ public class Main {
         System.out.println("Unsorted: " + Arrays.toString(nums2));
         SelectionSort.selectionSort(nums2);
         System.out.println("Sorted: " + Arrays.toString(nums2));
+        System.out.println("");
+        System.out.println("Unsorted: " + Arrays.toString(nums3));
+        InsertionSort.insertionSort(nums3);
+        System.out.println("Sorted: " + Arrays.toString(nums3));
     }
 }
