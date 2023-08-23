d73c89cc2d83cef9d9c223837d21e545f000ed7b
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-23 15:52:15 -0700

    sorts setup

---
+++ src/Sorts/Main.java
@@ -0,0 +1,19 @@
+package Sorts;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Main {
+
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>();
+        nums.add(12);
+        nums.add(21);
+        nums.add(39);
+        nums.add(16);
+        nums.add(90);
+        nums.add(1);
+
+        System.out.println("Unsorted: " + nums);
+    }
+}
