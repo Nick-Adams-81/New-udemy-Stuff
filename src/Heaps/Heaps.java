580a6cd400d0b4585e31f86cb31fb8b862fe471c
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-22 17:20:13 -0700

    heap helper methods

--- src/Heaps/Heaps.java
+++ src/Heaps/Heaps.java
@@ -1,6 +1,36 @@
 package Heaps;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Heaps {
 
+    private List<Integer> heap;
+
+    public Heaps() {
+        this.heap = new ArrayList<>();
+    }
+
+    public List<Integer> getHeap() {
+        return new ArrayList<>(heap);
+    }
+
+    private int leftChild(int index) {
+        return 2 * index + 1;
+    }
+
+    private int rightChild(int index) {
+        return 2 * index + 2;
+    }
+
+    private int parent(int index) {
+        return (index -1) / 2;
+    }
+
+    private void swap(int index1, int index2) {
+        int temp = heap.get(index1);
+        heap.set(index1, heap.get(index2));
+        heap.set(index2, temp);
+    }
 
 }
