1815474921f4fcfd8a57c4a5730e772551c24ad7
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-24 19:41:37 -0700

    pivot

--- src/Sorts/QuickSort.java
+++ src/Sorts/QuickSort.java
@@ -8,4 +8,17 @@ public class QuickSort {
         arr[secondIndex] = temp;
     }
 
+    private static int pivot(int[] arr, int pivotIndex, int endIndex) {
+        int swapIndex = pivotIndex;
+        for(int i = pivotIndex + 1; i <= endIndex; i++) {
+            if(arr[i] < arr[pivotIndex]) {
+                swapIndex++;
+                swap(arr, swapIndex, i);
+            }
+        }
+        swap(arr, pivotIndex, swapIndex);
+        return swapIndex;
+    }
+
+
 }
