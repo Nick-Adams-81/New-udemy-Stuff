5c9b38d9d43a8e8d8f81a2ec16ba04736b05a125
Nick Adams <nick.pappas.adams@gmail.com>
2023-08-25 10:32:01 -0700

    dfs post order

--- src/Trees/BST.java
+++ src/Trees/BST.java
@@ -152,4 +152,21 @@ public class BST {
         new Traverse(root);
         return results;
     }
+
+    public ArrayList<Integer> DFSPostOrder() {
+        ArrayList<Integer> results = new ArrayList<>();
+        class Traverse {
+            Traverse(Node currentNode) {
+                if(currentNode.left != null) {
+                    new Traverse(currentNode.left);
+                }
+                if(currentNode.right != null) {
+                    new Traverse(currentNode.right);
+                }
+                results.add(currentNode.value);
+            }
+        }
+        new Traverse(root);
+        return results;
+    }
 }
--- src/Trees/Main.java
+++ src/Trees/Main.java
@@ -18,6 +18,7 @@ public class Main {
 
         System.out.println("Breadth first search: " + tree.BFS());
         System.out.println("Depth first search pre order " + tree.DFSPreOrder());
+        System.out.println("Depth first search post order: " + tree.DFSPostOrder());
         System.out.println(tree.root.left.right.value);
         System.out.println(tree.contains(47));
         System.out.println(tree.contains(4));
